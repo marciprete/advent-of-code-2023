9c4d41394f977773060532350ce2b4c64dd7eceb
michele <michele.arciprete@e.dea.it>
2023-12-09 16:18:03 +0100

    Solution Day 7

--- src/Day07/Day07.kt
+++ src/Day07/Day07.kt
@@ -1,72 +1,70 @@
-class Hand(var hand: String, val win: Int) : Comparable<Hand> {
-    val shuffle = getPoints(hand)
-    val power = getPower(shuffle)
-//    val cards = listOf('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2')
-    val cards = listOf('A', 'K', 'Q',  'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J')
-
-    fun getPoints(hand: String) = hand.groupBy({ it }, { it }).map { (k, v) -> k to v.size }
-    fun getPower(pairs: List<Pair<Char, Int>>) = pairs.map{it.second}.fold(0) { acc, pair -> acc + (pair * pair) }
-
-    fun getJokerPower(): Int {
-        val nec = hand.groupBy({ it }, { it }).maxBy { (_,v) -> v.size }.key
-        return when (hand.contains('J')) {
-            true -> {
-                println("$hand to $nec ")
-                getPower(getPoints(hand.replace('J', nec)))
-            }//.replace('J', )
-            false -> power
-        }
-    }
-
-    override fun compareTo(o: Hand): Int {
-        val oList = this.hand.toList()
-        val itera = o.hand.toList().iterator().withIndex()
-        var comp: Int = 0
-        while (itera.hasNext()) {
-            val c = itera.next()
-            comp = cards.indexOf(c.value).compareTo(cards.indexOf(oList[c.index]))
-            if (comp != 0) break
-        }
-        return comp
-    }
-
-    override fun toString(): String {
-        return "Hand(hand='$hand', win=$win, shuffle=$shuffle, power=$power, cards=$cards)"
-    }
-
-}
-
+@OptIn(ExperimentalStdlibApi::class)
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        val list = input.map { line ->
-            val (hand, win) = line.trim().split(" ")
-            Hand(hand, win.toInt())
-        }.toList()
-        val sorted = list.sortedWith( compareBy<Hand>({it.power}, {it}))
+    val cards = listOf('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2')
+    val altCards = listOf('A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J')
 
-//        println(sorted.mapIndexed{ index, hand -> (index+1)*hand.win }.sum())
-        return 0
+    class Hand(val hand: String, val bid: Int) {
+        val value: String
+            get() {
+                return hand.map{ c -> cards.reversed().indexOf(c).toHexString()}.reduce{acc, s -> acc+s }
+            }
+
+        val altValue: String
+            get() {
+                return hand.map{ c -> altCards.reversed().indexOf(c).toHexString()}.reduce{acc, s -> acc+s }
+            }
+
+        val strength: Int
+            get() {
+                return hand.groupBy({ it }, { it }).map { (k, v) -> k to v.size }.fold(0) { acc, pair -> acc + pair.second * pair.second }
+            }
+
+        val altStrength: Int
+            get() {
+                val mostFreqChars = hand.groupBy({ it }, { it }).map { (k, v) -> k to v.size }.toList().sortedWith(
+                    compareBy({ it.second }, {altCards.reversed().indexOf(it.first)})
+                ).reversed()
+                var mostFreqChar = mostFreqChars.first()
+                if(mostFreqChar.first=='J' && mostFreqChar.second!=5) {
+                    mostFreqChar = mostFreqChars[1]
+                }
+                return hand.replace('J', mostFreqChar.first).groupBy({ it }, { it }).map { (k, v) -> k to v.size }.fold(0) { acc, pair -> acc + pair.second * pair.second }
+            }
+
+        override fun toString(): String {
+            return "Hand(hand='$hand', bid=$bid, strength=${strength}, altStr=${altStrength})"
+        }
+    }
+
+    fun mapInputToListOfHands(input: List<String>): List<Hand> {
+        return input.map { line ->
+            val (hand, bid) = line.split(" ")
+            Hand(hand, bid.toInt())
+        }
+    }
+
+    fun part1(input: List<String>): Int {
+       return mapInputToListOfHands(input).sortedWith(
+            compareBy<Hand>{it.strength}.thenBy { it.value }
+        ).mapIndexed{ index, hand -> (index+1)*hand.bid }.sum()
+
+
     }
 
     fun part2(input: List<String>): Int {
-        val list = input.map { line ->
-            val (hand, win) = line.trim().split(" ")
-            Hand(hand, win.toInt())
-        }.toList()
-        val sorted = list.sortedWith( compareBy<Hand>({it.getJokerPower()}, {it}))
-        println ("--part2--")
-        println(sorted.mapIndexed{ index, hand -> (index+1)*hand.win }.sum())
-        //246710247 too high
-        //246954468
-        return 0
+        return mapInputToListOfHands(input).sortedWith(
+            compareBy<Hand>{it.altStrength}.thenBy { it.altValue }
+        ).mapIndexed{ index, hand -> (index+1)*hand.bid }.sum()
+
     }
 
     val testInput = readInput("Day07_test")
-    check(part1(testInput) == 0)
-    check(part2(testInput) == 0)
+    check(part1(testInput) == 6440)
+    check(part2(testInput) == 5905)
 
     val input = readInput("Day07")
+
     part1(input).println()
     part2(input).println()
 }
